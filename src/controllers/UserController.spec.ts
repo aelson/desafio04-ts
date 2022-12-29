a431608b86300ecd70970c58282cb73fc6f536fa
Aelson Alves <aelson@cyno.ca>
2022-12-29 00:29:38 -0300

    Incluir os testes unitários pendentes no controller

--- src/controllers/UserController.spec.ts
+++ src/controllers/UserController.spec.ts
@@ -5,7 +5,8 @@ import { makeMockResponse } from "../__mocks__/mockResponse.mock";
 
 describe('UserController', () => {
     const mockUserService: Partial<UserService> = {
-        createUser: jest.fn()
+        createUser: jest.fn(),
+        getAllUsers: jest.fn()
     }
     
     const userController = new UserController(mockUserService as UserService);
@@ -21,5 +22,30 @@ describe('UserController', () => {
         userController.createUser(mockRequest, mockResponse)
         expect(mockResponse.state.status).toBe(201)
         expect(mockResponse.state.json).toMatchObject({ message: 'Usuário criado' })
+    })
+
+    it('Erro ao tentar inserir sem informar o nome', () => {
+        const mockRequest = {
+            body: {
+                email: 'nath@test.com'
+            }
+        } as Request
+        const mockResponse = makeMockResponse()
+        userController.createUser(mockRequest, mockResponse)
+        expect(mockResponse.state.status).toBe(400)
+        expect(mockResponse.state.json).toMatchObject({ message: 'Bad request! Name obrigatório' })
+    })
+
+    it('Service getAllUsers chamado', () => {
+        const mockRequest = {
+            body: {
+                name: 'Nath',
+                email: 'nath@test.com'
+            }
+        } as Request
+        const mockResponse = makeMockResponse()
+        userController.getAllUsers(mockRequest, mockResponse)
+        expect(mockUserService.getAllUsers).toBeCalled()
+        expect(mockResponse.state.status).toBe(200)
     })
 })
